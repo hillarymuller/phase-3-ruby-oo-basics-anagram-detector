71ca54b10a421e9e280229bc19d3d57b06a3e333
Hillary Muller <hillaryaward@gmail.com>
2022-08-15 13:59:17 -0500

    commit tree a4245fba1d95bb2758c65addb277f1a9429431cd

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
-# Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+    def match(array_of_words)
+        array_of_words.filter do |word|
+            word.chars.sort == @word.chars.sort
+        end
+        
+    end
+end
